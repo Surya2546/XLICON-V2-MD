8aa13d8a8f1311e4127a7198458e899207b3cc86
Chakri _1211 <69616499+Surya2546@users.noreply.github.com>
2024-06-16 10:53:09 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ import fetch from 'node-fetch'
 import axios from 'axios'
 
 global.owner = [
-  ['918360234087', 'WHITE', true], 
+  ['919963361946', 'CHK311', true], 
   ['233208179431', 'Abraham Dwamena', true],
   ['923184070915', '𝙎𝘼𝙇𝙈𝘼𝙉 𝘼𝙃𝙈𝘼𝘿 -(𝘼𝙝𝙢𝙢𝙞)', true], 
   ['94789481495', '𝙼𝚉𝙽 𝙺𝙸𝙽𝙶 ⚡', true],
@@ -15,8 +15,8 @@ global.owner = [
 ] //Number of owners
 
 global.mods = ['923184070915', '94789481495', '923184474176', '233531910067', '233508488192'] 
-global.prems = ['923184070915', '233208179431', '94789481495', '918360234087', '233508488192']
-global.allowed = ['923184070915', '233208179431', '94789481495', '918360234087', '233508488192']
+global.prems = ['923184070915', '233208179431', '94789481495', '919963361946', '233508488192']
+global.allowed = ['923184070915', '233208179431', '94789481495', '919963361946', '233508488192']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
 global.keysxxx = global.keysZens[Math.floor(global.keysZens.length * Math.random())]
 global.keysxteammm = ['29d4b59a4aa687ca', '5LTV57azwaid7dXfz5fzJu', 'cb15ed422c71a2fb', '5bd33b276d41d6b4', 'HIRO', 'kurrxd09', 'ebb6251cc00f9c63']
@@ -52,8 +52,8 @@ global.APIKeys = { // APIKey Here
 // Sticker WM
 global.botname = 'XLICON-V2'
 global.premium = 'true'
-global.packname = 'XLICON TEAM' 
-global.author = '@XLICON-V2' 
+global.packname = 'CHK311' 
+global.author = 'Made By CHK311' 
 global.menuvid = 'https://telegra.ph/file/325630f66abc968eda8e2.mp4'
 global.igfg = '▢✓ Follow Our channel\nhttps://whatsapp.com/channel/0029VaE8GbCDzgTILE7OtC3e\n' 
 global.dygp = 'https://chat.whatsapp.com/BfH0KLkICn2BjmGFMRcGMW'
